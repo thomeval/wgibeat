4138d101549701499f2fb10c1663d9702a7c938f
thomeval <thomeval@e47dc5de-8ca6-d832-b116-08b54517cb26>
2010-08-08 14:25:45 +0000

    Fixed song title metrics mixup in MainGameScreen.

--- trunk/WindowsGame1/WindowsGame1/Screens/OptionScreen.cs
+++ trunk/WindowsGame1/WindowsGame1/Screens/OptionScreen.cs
@@ -23,6 +23,8 @@ namespace WGiBeat.Screens
         private void BuildMenu()
         {
             var item = new MenuItem {ItemText = "Song Volume"};
+            item.AddOption("0%", "0.00001");
+
             for (int x = 1; x < 11; x++)
             {
                 item.AddOption(x + "0%", "" + x * 0.1);
