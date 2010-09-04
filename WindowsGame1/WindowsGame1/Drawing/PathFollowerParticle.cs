8aff2468975838833e54a026bd1b7375a49409b2
XcepticZoki@gmail.com <XcepticZoki@gmail.com@e47dc5de-8ca6-d832-b116-08b54517cb26>
2010-09-04 09:20:34 +0000

    Finished PathFollowerParticle. Must do testing for fringe cases.

--- WindowsGame1/WindowsGame1/Drawing/PathFollowerParticle.cs
+++ WindowsGame1/WindowsGame1/Drawing/PathFollowerParticle.cs
@@ -10,7 +10,7 @@ namespace WGiBeat.Drawing
 {
     class PathFollowerParticle : DrawableObject
     {
-        public Particle Particle { get; set; }
+        public Sprite Particle { get; set; }
         public Boolean Wrap { get; set; }
         public Double Position { get; set; }
         public Double StepSize { get; set; }
@@ -28,65 +28,112 @@ namespace WGiBeat.Drawing
         private int SegmentIndex { get; set; }
         private Double CORNER_SNAP_LENGTH = 0.01; //Or something. StepSize / 4 best guess.
 
-
+        private double _partX = 0.0;
+        private double _partY = 0.0;
 
         public PathFollowerParticle()
         {
             Init();
         }
 
+
+
         private void Init()
         {
-            SegmentIndex = PositionToSegment();
-            RecalcSegments();
-            MoveSize = TotalLength / (1 / StepSize);
-            CORNER_SNAP_LENGTH = MoveSize / 6;
+            InitDefaults();
+
+            if (Points.Count > 0)
+            {
+                SegmentIndex = PositionToSegment();
+                RecalcSegments();
+            }
+                //MoveSize = TotalLength / (1 / StepSize);
+                //CORNER_SNAP_LENGTH = MoveSize / 6;
+                //MoveSize = 50;
+            
         }
         
+        private void InitDefaults()
+        {
+
+            SegmentIndex = 0;
+
+            Particle = new Sprite() //temp
+            {
+                SpriteTexture = TextureManager.Textures["Particle_1"]
+            };
+
+            Wrap = true;
+            Position = 0.0;
+            StepSize = 0.001;
+            Points = new List<Vector2>(4);
+        }
 
         public override void Draw(SpriteBatch spriteBatch)
         {
 
             Particle.Draw(spriteBatch);
 
-            IncrementPosition();
+            if (Points.Count > 0)
+                IncrementPosition();
+            else
+            {
+                Console.WriteLine("No points");
+            }
 
         }
 
         private void IncrementPosition()
         {
-            try
+
+            Vector2 pointFrom = Points[SegmentIndex];
+            Vector2 pointTo = Points[SegmentIndex + 1];
+
+
+            //Console.WriteLine(MoveSize * XCoef[SegmentIndex]);
+            //Console.WriteLine(MoveSize * YCoef[SegmentIndex]);
+
+            _partX = (_partX - (MoveSize * XCoef[SegmentIndex]));
+            _partY = (_partY - (MoveSize * YCoef[SegmentIndex]));
+            Particle.X = (int) _partX;
+            Particle.Y = (int) _partY;
+            //Console.WriteLine(Particle.X + ":" + Particle.Y);
+
+            Vector2 curVector = new Vector2(Particle.X, Particle.Y);
+
+
+            if (CalcDistance(curVector, pointFrom) < CORNER_SNAP_LENGTH)
             {
-                Vector2 pointFrom = Points[SegmentIndex];
-                Vector2 pointTo = Points[SegmentIndex + 1];
-
-                Particle.X = (int) (pointFrom.X + (MoveSize * XCoef[SegmentIndex]));
-                Particle.Y = (int) (pointFrom.Y + (MoveSize * XCoef[SegmentIndex]));
-
-
-                Vector2 curVector = new Vector2(Particle.X, Particle.Y);
-
-
-                if (CalcDistance(curVector, pointFrom) < CORNER_SNAP_LENGTH)
-                {
-                    Particle.SetPosition(pointFrom);
-                }
-
-                if (CalcDistance(curVector, pointTo) < CORNER_SNAP_LENGTH)
-                {
-                    Particle.SetPosition(pointTo);
-                    SegmentIndex += 1;
-                    //Fix position
-                }
-
-                Position += StepSize;
-                if (Position >= 0)
-                    Position = 0;
-
-  
+                Particle.SetPosition(pointFrom);
             }
-            catch
-            {}
+
+            Console.WriteLine("Dist = " + CalcDistance(curVector, pointTo));
+            if (CalcDistance(curVector, pointTo) < 2) //CORNER_SNAP_LENGTH)
+            {
+                Console.WriteLine("Changed segment");
+                Particle.SetPosition(pointTo);
+
+                SegmentIndex += 1;
+                SegmentIndex = SegmentIndex % (Segments.Count - 1);
+                //Fix position
+            } else
+                //Console.WriteLine(SegmentIndex = PositionToSegment());
+
+                Console.WriteLine(Position);
+            Position += StepSize;
+            if (Position >= 1)
+            {
+                Position = 0;
+                SegmentIndex = 0;
+
+                _partX = Points[0].X;
+                _partY = Points[0].Y;
+                Particle.X = (int)_partX;
+                Particle.Y = (int)_partY;
+            }
+
+        //SegmentIndex = PositionToSegment();
+
         }
 
 
@@ -114,7 +161,7 @@ namespace WGiBeat.Drawing
         }
 
 
-        public Boolean CalcSnap()
+        private Boolean CalcSnap()
         {
             //if 
             return false; //NOT IMPLEMENTED
@@ -122,12 +169,29 @@ namespace WGiBeat.Drawing
 
         public void RecalcSegments()
         {
+            Particle.X = (int)Points[0].X;
+            Particle.Y = (int)Points[0].Y;
+            _partX = Points[0].X;
+            _partY = Points[0].Y;
+            CORNER_SNAP_LENGTH = MoveSize / 6;
+            var a  = Points.Count;
+            Segments = new List<double>((Points.Count - (Points.Count % 2)));
+            XCoef = new List<double>(Segments.Count);
+            YCoef = new List<double>(Segments.Count);
+
+            for (int i = 0; i < a; i ++)
+            {
+                Segments.Add(0.0);
+                XCoef.Add(0.0);
+                YCoef.Add(0.0);
+            }
+
             Double totalLength = 0;
-            for (int i = 0; i < Points.Count; i++) //Working
+            for (int i = 0; i < Segments.Count - 1; i++) //Working
                 totalLength += RecalcSegment(i);
 
             TotalLength = totalLength;
-            
+            MoveSize = TotalLength / (1 / StepSize);
         }
 
         public Double RecalcSegment(int segmentIndexHint)
