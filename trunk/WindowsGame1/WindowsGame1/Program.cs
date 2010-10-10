1af6af2f309dfbaef58b4c56ea884a0afd1d76e5
thomeval <thomeval@e47dc5de-8ca6-d832-b116-08b54517cb26>
2010-10-10 13:25:12 +0000

    Added player indicators to beatline meters for easier identification. Added critical error handling - all exceptions cause an error message to be displayed, and the details are saved to file, instead of the generic "A problem has occurred" windows default.

--- trunk/WindowsGame1/WindowsGame1/Program.cs
+++ trunk/WindowsGame1/WindowsGame1/Program.cs
@@ -1,5 +1,6 @@
 using System;
-
+using System.Windows.Forms;
+using System.IO;
 namespace WGiBeat
 {
     static class Program
@@ -9,10 +10,44 @@ namespace WGiBeat
         /// </summary>
         static void Main(string[] args)
         {
-            using (GameCore game = new GameCore())
+            try
             {
-                game.Run();
+                using (GameCore game = new GameCore())
+                {
+                    game.Run();
+                }
             }
+             catch (Exception ex)
+            {
+                MessageBox.Show(
+                    "A critical error has occurred. Please check the error.txt file created in the wgibeat folder for details. If this is a bug, please send the error.txt to a developer.");
+                SaveErrorLog(ex);
+#if (DEBUG)
+                throw;
+#endif
+
+            }
+        }
+
+        private static void SaveErrorLog(Exception ex)
+        {
+            try
+            {
+                var file = new StreamWriter(File.Open("error.txt", FileMode.OpenOrCreate, FileAccess.Write));
+                file.WriteLine(ex.Message + "\n");
+                file.WriteLine(ex.StackTrace);
+                if (ex.InnerException != null)
+                {
+                    file.WriteLine(ex.InnerException.Message);
+                }
+            file.Close();
+            }
+            catch (Exception ex2)
+            {
+                MessageBox.Show("Failed to write error.txt file. " + ex2.Message + "\n Original wgibeat error: " +
+                                ex.Message + "\n" + ex.StackTrace);
+            }
+
         }
     }
 }
