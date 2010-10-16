326d12523c4ae6f850736c58afba1dd6009b604e
thomeval <thomeval@e47dc5de-8ca6-d832-b116-08b54517cb26>
2010-10-16 05:48:55 +0000

    Added instructions to TeamSelectScreen, as well as "one team has no players" error message.

--- trunk/WindowsGame1/WindowsGame1/Screens/TeamSelectScreen.cs
+++ trunk/WindowsGame1/WindowsGame1/Screens/TeamSelectScreen.cs
@@ -20,6 +20,10 @@ namespace WGiBeat.Screens
 
         private bool[] _ready = new bool[4];
         private readonly List<PlayerOptionsFrame> _playerOptions = new List<PlayerOptionsFrame>();
+        private string _restrictionMessage = "";
+        private bool _showWarningIcon;
+        private Sprite _messageBorder;
+        private Sprite _restrictionIcon;
 
         public TeamSelectScreen(GameCore core) : base(core)
         {
@@ -45,6 +49,8 @@ namespace WGiBeat.Screens
                     frameCount++;
                 }
             }
+            SetRestrictionMessage("Press left or right to \nchoose a team. Press start \nto confirm selection.", false);
+
         }
         public void InitSprites()
         {
@@ -64,13 +70,17 @@ namespace WGiBeat.Screens
 
             _playerMarkers = new SpriteMap
                                  {SpriteTexture = TextureManager.Textures["PlayerTeamMarkers"], Columns = 1, Rows = 4};
+            _messageBorder = new Sprite {SpriteTexture = TextureManager.Textures["MessageBorder"]};
+            _messageBorder.SetPosition(Core.Metrics["MessageBorder", 0]);
+            _restrictionIcon = new Sprite() {SpriteTexture = TextureManager.Textures["RestrictionIcon"], Width = 48, Height = 48};
+            _restrictionIcon.SetPosition(_messageBorder.X + 7, _messageBorder.Y + 7);
         }
         public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
         {
-
             DrawBackground(spriteBatch);
             DrawMarkers(spriteBatch);
             DrawPlayerOptions(spriteBatch);
+            DrawRestrictionMessage(spriteBatch);
         }
 
         private void DrawPlayerOptions(SpriteBatch spriteBatch)
@@ -116,6 +126,18 @@ namespace WGiBeat.Screens
             _backgroundSprite.Draw(spriteBatch);
             _headingSprite.Draw(spriteBatch);
             _teamBorderSprite.Draw(spriteBatch);
+        }
+
+        private void DrawRestrictionMessage(SpriteBatch spriteBatch)
+        {
+            if (_restrictionMessage != "")
+            {
+                _messageBorder.Draw(spriteBatch);
+                TextureManager.DrawString(spriteBatch, _restrictionMessage, "DefaultFont", Core.Metrics["RestrictionMessage", 0], Color.White, FontAlign.LEFT);
+                if (_showWarningIcon)
+                    _restrictionIcon.Draw(spriteBatch);
+            }
+
         }
 
         public override void PerformAction(Action action)
@@ -213,21 +235,37 @@ namespace WGiBeat.Screens
 
         private void TryToStart()
         {
+
             var canStart = true;
             for (int x = 0; x < 4; x++)
             {
                 canStart = canStart && !(Core.Players[x].Playing ^ _ready[x]);
             }
+            if (!canStart)
+            {
+                SetRestrictionMessage("Press left or right to \nchoose a team. Press start \nto confirm selection.", false);
+                return;
+            }
             var blueTeamCount = (from e in Core.Players where e.Team == 1 select e).Count();
             var redTeamCount = (from e in Core.Players where e.Team == 2 select e).Count();
 
-            canStart = canStart && (blueTeamCount > 0);
+            canStart = (blueTeamCount > 0);
             canStart = canStart && (redTeamCount > 0);
 
-            if (canStart)
+            if (!canStart)
+            {
+                SetRestrictionMessage("One team has no players.", true);
+            }  
+            else
             {
                 Core.ScreenTransition("SongSelect");
             }
+        }
+
+        private void SetRestrictionMessage(string message, bool isProblem)
+        {
+            _restrictionMessage = message;
+            _showWarningIcon = isProblem;
         }
     }
 }
