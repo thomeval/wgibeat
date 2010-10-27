c314c86997d59e9ffe7bfb1deb90a228c3a178a5
thomeval <thomeval@e47dc5de-8ca6-d832-b116-08b54517cb26>
2010-10-27 15:01:32 +0000

    Implemented a MovingBackground object for InstructionScreen. Likely to be used in other screens later as well.

---
+++ trunk/WindowsGame1/WindowsGame1/Drawing/MovingBackground.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace WGiBeat.Drawing
+{
+    public class MovingBackground : Sprite
+    {
+        public double Direction { get; set; }
+
+        private double _speed = 1;
+        public double Speed
+        {
+            get { return _speed; }
+            set { _speed = value; }
+        }
+
+        private double _offsetX;
+        private double _offsetY;
+
+ 
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            Move();
+
+            base.DrawTiled(spriteBatch,(int) _offsetX,(int) _offsetY, Width, Height);
+        }
+
+        private void Move()
+        {
+            if (this.SpriteTexture != null)
+            {
+                _offsetX = (_offsetX + Math.Sin(Direction)*Speed) % Width;
+                _offsetY = (_offsetY + Math.Cos(Direction) * Speed) % Height;
+            }
+        }
+    }
+}
