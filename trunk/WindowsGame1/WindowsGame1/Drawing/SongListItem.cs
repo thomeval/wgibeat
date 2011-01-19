32d9d92454e91706878904e827496fc123e2c4f4
thomeval <thomeval@e47dc5de-8ca6-d832-b116-08b54517cb26>
2011-01-19 12:09:28 +0000

    Improved SpriteBatch to be able to automatically determine Drawing width and height. Duplicated MainMenuOption graphic as SongListItem (for theming). Improved player Initialization code in GameCore.

--- trunk/WindowsGame1/WindowsGame1/Drawing/SongListItem.cs
+++ trunk/WindowsGame1/WindowsGame1/Drawing/SongListItem.cs
@@ -8,14 +8,20 @@ namespace WGiBeat.Drawing
     {
         public GameSong Song { get; set; }
         public bool IsSelected { get; set; }
+
+        private readonly SpriteMap _itemSpriteMap;
+
+        public SongListItem()
+        {
+            _itemSpriteMap = new SpriteMap
+                                {Columns = 1, Rows = 2, SpriteTexture = TextureManager.Textures["SongListItem"]};
+        }
         public override void Draw(SpriteBatch spriteBatch)
         {
             
             //Draw Base
-            var baseSprite = new Sprite {Height = this.Height, Width = this.Width, X = this.X, Y = this.Y};
-            baseSprite.SpriteTexture = IsSelected ? TextureManager.Textures["mainMenuOptionSelected"] : TextureManager.Textures["mainMenuOption"];
-            
-            baseSprite.Draw(spriteBatch);
+            var idx = IsSelected ? 1 : 0;
+            _itemSpriteMap.Draw(spriteBatch,idx,this.X,this.Y);
             //Draw Text
             var textPosition = new Vector2(this.X + 20, this.Y + 3);
             spriteBatch.DrawString(TextureManager.Fonts["LargeFont"], Song.Title, textPosition,Color.Black);
