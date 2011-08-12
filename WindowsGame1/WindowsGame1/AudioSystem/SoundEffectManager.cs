f1879e5163fb6b35f942ee6f4444af0b2139bcc2
thomeval <thomeval@e47dc5de-8ca6-d832-b116-08b54517cb26>
2011-08-12 15:36:11 +0000

    Beginning of SoundEffectManager for menu sound effects.

---
+++ WindowsGame1/WindowsGame1/AudioSystem/SoundEffectManager.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using WGiBeat.Managers;
+
+namespace WGiBeat.AudioSystem
+{
+    public class SoundEffectManager : Manager
+    {
+        public AudioManager AudioManager { get; set; }
+        public SettingsManager SettingsManager { get; set; }
+        public Dictionary<string, string> Sounds;
+
+        public SoundEffectManager(LogManager log, AudioManager audioManager, SettingsManager settings)
+        {
+            Log = log;
+            AudioManager = audioManager;
+            SettingsManager = settings;
+            Sounds = new Dictionary<string, string>();
+            Log.AddMessage("Initializing Song Manager...", LogLevel.INFO);
+        }
+
+        public void LoadFromFile(string filename)
+        {
+            //TODO: Define file format.
+        }
+    }
+
+    public enum SoundEvent
+    {
+        MAIN_MENU_DECIDE,
+        MAIN_MENU_BACK,
+        MAIN_MENU_SELECT_UP,
+        MAIN_MENU_SELECT_DOWN,
+        MENU_DECIDE,
+        MENU_BACK,
+        MENU_SELECT_UP,
+        MENU_SELECT_DOWN,
+        MENU_OPTION_SELECT_LEFT,
+        MENU_OPTION_SELECT_RIGHT,
+        PLAYER_OPTIONS_DISPLAY,
+        PLAYER_OPTIONS_HIDE,
+        PLAYER_OPTIONS_UP,
+        PLAYER_OPTIONS_DOWN,
+        SONG_DECIDE,
+        SONG_SELECT_UP,
+        SONG_SELECT_DOWN,
+        SONG_SORT_SELECT_UP,
+        SONG_SORT_SELECT_DOWN,
+        SONG_SORT_SELECT_LEFT,
+        SONG_SORT_SELECT_RIGHT,
+        SONG_SORT_DISPLAY,
+        SONG_SORT_HIDE
+    }
+}
